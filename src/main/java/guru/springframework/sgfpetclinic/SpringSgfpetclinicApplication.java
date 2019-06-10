cac4540fa96392bd9465bfb506cd29e09dfd9553
Clement TRIBOUILLARD <clement.tribouillard@siicanada.com>
2019-06-10 14:56:47 -0400

    first commit

---
+++ src/main/java/guru/springframework/sgfpetclinic/SpringSgfpetclinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sgfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSgfpetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSgfpetclinicApplication.class, args);
+	}
+
+}
